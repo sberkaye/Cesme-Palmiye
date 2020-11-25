e24046474d7232dc39a1509bdf1668cc9e1d4b6e
sberkaye <berkayergin99@gmail.com>
2020-11-25 12:59:44 +0300

    Fixing typos

--- js/responsiveNav.js
+++ js/responsiveNav.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector(".header__nav--responsive");
 const items = Array.from(document.querySelectorAll(".main")); // get the main nav items
-console.log(items);
+// console.log(items);
 menu.addEventListener("click", () => {
   items.forEach((item) => {
     item.classList.toggle("main");
